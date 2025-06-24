939a7c6338261ce571ddf3d278fab1c883ef9361
AnshMNSoni <anshsoni702@gmail.com>
2025-06-24 20:03:29 +0530

    add responsive ness for some devices

--- app_clone/lib/main.dart
+++ app_clone/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app_clone/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +12,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, 
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark),
-      home: Home(),
+      home: AnimatedSplashScreen(
+        duration: 1000,
+        splash: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+          ),
+          child: Image.asset("assets/app_logo.png"),
+        ),
+        nextScreen: Home(),
+        splashTransition: SplashTransition.fadeTransition,
+        backgroundColor: Colors.black,
+      ),
     );
   }
 }

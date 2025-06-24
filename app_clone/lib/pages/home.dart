e3ca911b7e97171319b4f6996306a4493811318e
AnshMNSoni <anshsoni702@gmail.com>
2025-06-24 12:00:06 +0530

    tickets section completed

--- app_clone/lib/pages/home.dart
+++ app_clone/lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:app_clone/pages/appdrawer.dart';
 import 'package:app_clone/pages/express.dart';
 import 'package:app_clone/pages/pnr.dart';
 import 'package:app_clone/pages/tickets.dart';
@@ -16,20 +17,6 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   int selectBottomNavIndex = 0;
   PageController bottomNavPageController = PageController();
-
-  Map<Icon, String> drawerListTileItems = {
-    Icon(Icons.update): "Update Timetable",
-    Icon(Icons.language): "Language",
-    Icon(Icons.location_city): "Change city",
-    Icon(Icons.dark_mode): "Change light/dark mode",
-    Icon(Icons.cancel): "Clear Recent Searches",
-    Icon(Icons.settings): "Settings",
-    Icon(Icons.share): "Share App",
-    Icon(Icons.rate_review): "Rate Us",
-    Icon(Icons.report): "Report Issue",
-    Icon(Icons.settings_suggest_outlined): "Suggest a feature",
-    Icon(Icons.notifications): "View all Alerts",
-  };
 
   // Tab Controller Functionality
   @override
@@ -55,97 +42,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        elevation: 0,
-        shadowColor: Colors.black,
-        surfaceTintColor: Colors.black,
-        width: MediaQuery.of(context).size.width * 0.85,
-        child: Container(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                padding: EdgeInsets.zero,
-                margin: EdgeInsets.zero,
-                child: Container(
-                  padding: EdgeInsets.only(
-                    left: MediaQuery.of(context).size.height * 0.035,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: Colors.white,
-                            radius: MediaQuery.of(context).size.height * 0.05,
-                            backgroundImage: AssetImage("assets/app_logo.png"),
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.height * 0.028,
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                "Where is my Train",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              Text(
-                                "from Google",
-                                style: TextStyle(
-                                  fontStyle: FontStyle.italic,
-                                  color: Colors.grey.shade400,
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          top: MediaQuery.of(context).size.height * 0.035,
-                          right: MediaQuery.of(context).size.height * 0.035,
-                        ),
-                        child: Text(
-                          "Last Sync: 23-June-2025 10:55",
-                          style: TextStyle(
-                            color: Color(0xFFc0caef),
-                            fontSize: 12,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              for (int i = 0; i < 11; i++)
-                Container(
-                  color: AppColors.drawerListTileColor,
-                  padding: EdgeInsets.only(
-                    left: MediaQuery.of(context).size.height * 0.035,
-                  ),
-                  child: ListTile(
-                    leading: drawerListTileItems.keys.elementAt(i),
-                    title: Text(
-                      drawerListTileItems.values.elementAt(i),
-                      style: TextStyle(
-                        color: AppColors.drawerFontColor,
-                        fontSize: 19,
-                      ),
-                    ),
-                  ),
-                ),
-            ],
-          ),
-        ),
-      ),
+      drawer: AppDrawer(),
       appBar: AppBar(
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
--- app_clone/lib/pages/tickets.dart
+++ app_clone/lib/pages/tickets.dart
@@ -11,12 +11,14 @@ class TicketsPage extends StatefulWidget {
 class _TicketsPageState extends State<TicketsPage> {
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
+    final h = MediaQuery.of(context).size.height;
+    final w = MediaQuery.of(context).size.width;
+
+    Map _cityList = {"GNC": "Gandhinagar Capital", "": "", "NVS": "Navsari"};
 
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.arrow_left, size: screenWidth * 0.08),
+        leading: Icon(Icons.arrow_left, size: w * 0.08),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -24,18 +26,341 @@ class _TicketsPageState extends State<TicketsPage> {
               "TICKETS",
               style: TextStyle(
                 color: Colors.white,
-                fontSize: screenWidth * 0.05,
+                fontSize: w * 0.05,
                 fontWeight: FontWeight.w500,
               ),
             ),
-            Icon(
-              Icons.more_vert,
-              size: screenWidth * 0.07,
-              color: Colors.white,
-            ),
+            Icon(Icons.more_vert, size: w * 0.07, color: Colors.white),
           ],
         ),
         backgroundColor: AppColors.appbarColor,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: w * 0.05, bottom: w * 0.05),
+            decoration: BoxDecoration(
+              color: AppColors.containerColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(4),
+                topRight: Radius.circular(4),
+              ),
+            ),
+            margin: EdgeInsets.only(
+              top: w * 0.035,
+              left: w * 0.02,
+              right: w * 0.02,
+            ),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        Icons.circle_outlined,
+                        color: Colors.grey.shade600,
+                        size: 16,
+                      ),
+
+                      SizedBox(height: w * 0.01),
+
+                      for (int dot = 0; dot <= 9; dot++)
+                        Icon(
+                          Icons.circle,
+                          size: 4,
+                          color: Colors.grey.shade600,
+                        ),
+
+                      Icon(Icons.arrow_downward, color: Colors.grey.shade600),
+
+                      SizedBox(height: w * 0.01),
+
+                      Icon(
+                        Icons.circle_outlined,
+                        color: Colors.grey.shade600,
+                        size: 16,
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    for (int item = 0; item <= 2; item++) ...[
+                      if (item % 2 == 0)
+                        Row(
+                          spacing: w * 0.23,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              width: w * 0.55,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: AppColors.tagColor,
+                                      borderRadius: BorderRadius.circular(2),
+                                    ),
+                                    padding: EdgeInsets.only(
+                                      left: w * 0.009,
+                                      right: w * 0.009,
+                                    ),
+                                    child: Text(
+                                      _cityList.keys.elementAt(item),
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ),
+
+                                  SizedBox(width: w * 0.02),
+
+                                  Container(
+                                    child: Text(
+                                      _cityList.values.elementAt(item),
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Icon(Icons.cancel_outlined, size: 20),
+                          ],
+                        )
+                      else
+                        Stack(
+                          alignment: AlignmentGeometry.directional(0.7, 0),
+                          children: [
+                            Container(
+                              width: w * 0.8,
+                              height: w * 0.002,
+                              color: Colors.grey.shade600,
+                            ),
+                            Container(
+                              width: w * 0.13,
+                              height: w * 0.13,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: Colors.grey.shade600,
+                                  width: 1,
+                                ),
+                              ),
+                              child: CircleAvatar(
+                                radius: w * 0.065,
+                                backgroundColor: AppColors.containerColor,
+                                child: Icon(
+                                  Icons.compare_arrows,
+                                  color: Colors.greenAccent,
+                                  size: w * 0.08,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          // Custom Divider()
+          Container(
+            margin: EdgeInsets.only(left: w * 0.02, right: w * 0.02),
+            width: w,
+            height: w * 0.002,
+            color: Colors.grey.shade600,
+          ),
+
+          Container(
+            color: AppColors.containerColor,
+            margin: EdgeInsets.only(left: w * 0.02, right: w * 0.02),
+            padding: EdgeInsets.all(w * 0.04),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.calendar_month_outlined,
+                      color: AppColors.tagColor,
+                      size: 14,
+                    ),
+
+                    SizedBox(width: w * 0.015),
+
+                    Text(
+                      "Journey Date",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: w * 0.02),
+
+                Text(
+                  "23 June, Monday",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 21,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Cutom Divider()
+          Container(
+            margin: EdgeInsets.only(left: w * 0.02, right: w * 0.02),
+            width: w,
+            height: w * 0.002,
+            color: Colors.grey.shade600,
+          ),
+
+          Container(
+            color: AppColors.containerColor,
+            margin: EdgeInsets.only(left: w * 0.02, right: w * 0.02),
+            padding: EdgeInsets.all(w * 0.04),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.confirmation_num_outlined,
+                      color: AppColors.tagColor,
+                      size: 15,
+                    ),
+
+                    SizedBox(width: w * 0.015),
+
+                    Text(
+                      "Quota",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: w * 0.02),
+
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.tagColor,
+                        borderRadius: BorderRadius.circular(2),
+                      ),
+                      padding: EdgeInsets.only(
+                        top: w * 0.008,
+                        bottom: w * 0.008,
+                        left: w * 0.03,
+                        right: w * 0.03,
+                      ),
+                      child: Text(
+                        "GN",
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                    ),
+
+                    SizedBox(width: w * 0.02),
+
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            width: w * 0.002,
+                            color: Colors.grey.shade600,
+                          ),
+                        ),
+                      ),
+                      width: w * 0.64,
+                      child: Text(
+                        "General",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(width: w * 0.04),
+
+                    Icon(Icons.arrow_drop_down),
+                  ],
+                ),
+
+                SizedBox(height: w * 0.03),
+
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.buttonColor,
+                      padding: EdgeInsets.only(
+                        left: w * 0.25,
+                        right: w * 0.25,
+                        top: w * 0.025,
+                        bottom: w * 0.025,
+                      ),
+                    ),
+                    child: Text(
+                      "Find PNR Status",
+                      style: TextStyle(
+                        fontSize: w * 0.05,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: AppColors.containerColor,
+            ),
+            margin: EdgeInsets.only(
+              top: w * 0.02,
+              left: w * 0.02,
+              right: w * 0.02,
+            ),
+            padding: EdgeInsets.only(top: w * 0.02, bottom: w * 0.02),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Your tickets on Confirmtkt",
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: AppColors.tagColor,
+                    fontSize: 19,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(width: MediaQuery.of(context).size.width * 0.13),
+                Icon(Icons.arrow_right, color: AppColors.tagColor, size: 36),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
